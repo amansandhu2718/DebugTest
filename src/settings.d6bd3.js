071a53274d12c6dd54d194ea8508827a85de9de9
Amandeep Singh <a9560301656@gmail.com>
2022-05-01 01:28:02 -0700

    test3

---
+++ src/settings.d6bd3.js
@@ -0,0 +1,29 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "Objects",
+        "Walls"
+    ],
+    collisionMatrix: [
+        [
+            true,
+            true
+        ],
+        [
+            true,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/Game.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "9d52b",
+        main: "52bdf"
+    }
+};
